b8122636765b2258c0dc2bd2e0392a239085d25e
Raihan <raihanuddinahmed10@gmail.com>
2025-07-02 23:42:43 +0600

    our partnar complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,3 +6,53 @@ module.exports = {
   },
   plugins: [],
 };
+
+// <!-- Market Making for Crypto Exchanges -->
+//     <section class="bg-[#141619] py-28">
+//       <div class="container max-w-screen-lg mx-auto">
+//         <div class="text-center">
+//           <h2 class="text-center text-3xl lg:text-6xl text-white">
+//             Crypto Market Making
+//           </h2>
+//           <p class="text-[#E5E5E5] py-5 text-lg lg:text-xl">
+//             We are a global crypto liquidity provider and algorithmic market
+//             maker. <br />
+//             We trade digital assets listed on Centralized Exchanges in
+//             <br />over 15 countries worldwide.
+//           </p>
+//         </div>
+//       </div>
+
+//       <!-- images and text -->
+//       <div class="container max-w-screen-lg mx-auto flex justify-between items-center py-10">
+//          <!-- image -->
+//         <div>
+//           <img class="" src="./images/" alt="" />
+//         </div>
+
+//         <!-- text -->
+//         <div class="">
+//           <h2 class="text-white text-3xl lg:text-5xl">
+//             Market Making for <br />
+//             Crypto Projects
+//           </h2>
+//           <h4 class="text-white font-semibold py-5 text-2xl">
+//             Accelerate your token’s journey by <br />
+//             boosting its liquidity
+//           </h4>
+//           <p class="text-[#E5E5E5]">
+//             We invest in building long-term, <br />
+//             sustainable relationships and support our <br> projects in their growth
+//             journey with our <br />services, industry expertise and network.
+//           </p>
+//           <div class="py-5">
+//             <a href="#">
+//               <button class="text-white underline underline-offset-4 text-2xl">
+//                 Learn More
+//               </button>
+//             </a>
+//           </div>
+//         </div>
+
+//       </div>
+//     </section>
